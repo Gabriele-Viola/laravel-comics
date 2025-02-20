def9a49c3053197977f6b1186a8c978d0cdbfbd8
Gabriele-Viola <gabrieleviola50@gmail.com>
2025-02-20 12:33:05 +0100

    add card component

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -2,9 +2,23 @@
 @section('title')
     Dc comics
 @endsection
-
+@php
+    $comics= config("comics");
+@endphp
 @section('content')
  <div class="container">
      <h1>prendo il layout main</h1>
+     <div class="row row-cols-6">
+      @foreach ($comics as $comic)
+          <div class="col">
+              <x-card>
+                <x-slot:title>
+                    {{$comic['title']}}
+                </x-slot:title>
+              </x-card>
+
+          </div>
+      @endforeach
+     </div>
     </div>   
 @endsection
---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
+    <h3>{{$title}}</h3>
+    {{$slot}}
+</div>
