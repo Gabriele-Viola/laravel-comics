daffabcb69a854201f5673448366cab7690f7256
Gabriele-Viola <gabrieleviola50@gmail.com>
2025-02-20 12:08:12 +0100

    add layout n img into folder

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,135 @@
+<footer>
+    <div class="container">
+        <div class="blu-band">
+            <div>
+                logo
+            </div>
+            <div>
+                digital comics
+            </div>
+            <div>
+                logo
+            </div>
+            <div>
+                DC merch
+            </div>
+        
+            <div>
+                logo
+            </div>
+            <div>
+                subscription
+            </div>
+        
+            <div>
+                logo
+            </div>
+            <div>
+                comic shop location
+            </div>
+        
+            <div>
+                logo
+            </div>
+            <div>
+                dc power visa
+            </div>
+        </div>
+        
+        <div class="info">
+            <div class="col1">
+                <h4>DC comics</h4>
+                <ul>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                </ul>
+                <h4>shop</h4>
+                <ul>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col2">
+                <h4>DC</h4>
+                <ul>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col3">
+                <h4>sites</h4>
+                <ul>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                    <li>
+                        <a href="">lorem</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,37 @@
+<header class="container d-flex justify-content-between">
+    <div>Logo</div>
+    <nav class="">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href=""> characters</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">comics</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">movies</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">TV</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">games</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">collectibles</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">videos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">fans</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">news</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">shop</a>
+            </li>
+        </ul>
+    </nav>
+</header>
